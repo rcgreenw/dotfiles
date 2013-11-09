44cfd0b0fd24d18995026b881b53664384c0d420
rcgreenw <rcgreenw@raven.cluster.tausd.org>
2013-11-08 17:46:17 -0800

    Fixed and improved mkLinks.sh
    - File exist check looks in repo directory.
    
    - Correctly skips existing symlinks.

--- mkLinks.sh
+++ mkLinks.sh
@@ -3,25 +3,26 @@
 SCRIPTPATH=$(dirname `which ${BASH_SOURCE[0]}`)
 cd ~/
 for file in `grep -v ^# $SCRIPTPATH/files` ; do
-   if [[ -a $file ]] ; then
+   if [[ -a $SCRIPTPATH/$file ]] ; then
       # Only proceed if source file exists
       dir=`dirname $file`
       if [[ ! -d $dir ]] ; then
          mkdir -p $dir
       fi
-      if [[ -L $file ]] ; then
-         # Skip
-         echo Skipping $file
-      elif [[ -f $file ]] ; then
-         # Backup then link
-         mv $file $SCRIPTPATH/$file.mkLinksBackup
-         ln -sf $SCRIPTPATH/$file $file
-      elif [[ ! -a $file ]] ; then
-         # Link
-         ln -sf $SCRIPTPATH/$file $file
-      else
-         # Unknown
-         echo Unknown file in the way: $file
+      if [[ ! -L $file ]] ; then
+         # Link doesn't exist, do something
+         if [[ -f $file ]] ; then
+            # Backup then link
+            echo Backup up $file to $SCRIPTPATH/$file.mkLinksBackup
+            mv $file $SCRIPTPATH/$file.mkLinksBackup &&
+            ln -sf $SCRIPTPATH/$file $file
+         elif [[ -a $file ]] ; then
+            # Unknown
+            echo Unknown file in the way: $file
+         else
+            # Link
+            ln -sf $SCRIPTPATH/$file $file
+         fi
       fi
    else
       # No source file
