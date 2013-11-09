ddc6083d5f0ac588c418dee4de8eae7511152000
Richard C. Greenwood <rcgreenw@tcoek12.org>
2013-11-08 17:09:47 -0800

    Updated mkLinks.sh to deal with more cases.
    - Handles running from different directories.
    
    - Skips files that don't exist in repo.  Lists them as a reminder to
      remove them from files list.
    
    - Copies files that do exist in home directory to repo
      with mkLinksBackup extension for easy diff/merge.
    
    - Skips and lists files that exist in home directory but aren't
      symlinks or regular files.

--- mkLinks.sh
+++ mkLinks.sh
@@ -1,12 +1,30 @@
 #! /bin/bash
 
+SCRIPTPATH=$(dirname `which ${BASH_SOURCE[0]}`)
 cd ~/
-for file in `cat .gw/dotfiles/files` ; do
-   dir=`dirname $file`
-   if [[ ! -d $dir ]] ; then
-      mkdir -p $dir
+for file in `grep -v ^# $SCRIPTPATH/files` ; do
+   if [[ -a $file ]] ; then
+      # Only proceed if source file exists
+      dir=`dirname $file`
+      if [[ ! -d $dir ]] ; then
+         mkdir -p $dir
+      fi
+      if [[ -L $file ]] ; then
+         # Skip
+         echo Skipping $file
+      elif [[ -f $file ]] ; then
+         # Backup then link
+         mv $file $SCRIPTPATH/$file.mkLinksBackup
+         ln -sf $SCRIPTPATH/$file $file
+      elif [[ ! -a $file ]] ; then
+         # Link
+         ln -sf $SCRIPTPATH/$file $file
+      else
+         # Unknown
+         echo Unknown file in the way: $file
+      fi
+   else
+      # No source file
+      echo File missing: $file
    fi
-   if [[ ! -L $file ]] ; then
-      ln -sf ~/.gw/dotfiles/$file $file
-   fi 
 done
